a1141b3fe84b8b35d462163ec7e340d9fd8eaacd
gabriellazanin <149081568+gabriellazanin@users.noreply.github.com>
2023-12-14 14:51:17 +0000

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Site de Exemplo', //nome do trabalho
-  tagline: 'Fazer para aprender',  //slogan
+  title: 'Oficina Mecânica, Lda.', //nome do trabalho
+  tagline: 'Oferencendo sempre o melhor serviço para os clientes!',  //slogan
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
